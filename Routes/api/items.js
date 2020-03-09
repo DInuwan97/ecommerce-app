594cc4d04e060812d56168ca17708a10dd6ba034
Dinuka Shameera <dinukashameera0657@gmail.com>
2020-03-10 00:05:34 +0530

    created the post request in Item route

--- Routes/api/items.js
+++ Routes/api/items.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Item = require("../../models/Item");
 
+//getting all the items
+//public access
 router.get("/", async (req, res) => {
   try {
     const Items = await Item.find();
@@ -16,5 +18,67 @@ router.get("/", async (req, res) => {
     res.status(500).json({ msg: "Server Error" });
   }
 });
+
+//getting a specific item
+//publuic access
+router.get("/:id", async (req, res) => {
+  try {
+    const item = await Item.findById({ _id: req.params.id });
+    if (!item) {
+      return res.status(400).send({ msg: "No item found" });
+    }
+    res.json(item);
+    console.log(item);
+  } catch (error) {
+    res.status(500).json({ msg: "Sever Error" });
+    console.error(error);
+  }
+});
+
+
+//adding a new item to the store
+//access private
+router.post('/',async (req,res) => {
+    let {itemName,price,category,size,color,Brand,stockQuantity,discount,rating} = req.body
+    const checkItemExist = await Item.findOne({itemName});
+    let result = null;
+    let newItem = null;
+    try {
+
+    if(checkItemExist){
+        stockQuantity = parseInt(checkItemExist.stockQuantity) + parseInt(stockQuantity);
+         newItem = await  Item.findOneAndUpdate({itemName},{
+            itemName,
+            price,
+            category,
+            size,
+            color,
+            Brand,
+            stockQuantity,
+            discount,
+            rating
+        });
+
+    }else{
+         newItem = await new Item({
+            itemName,
+            price,
+            category,
+            size,
+            color,
+            Brand,
+            stockQuantity,
+            discount,
+            rating
+        });
+    }
+   
+        result = await newItem.save();
+        console.log(result);
+        res.json(result)
+    } catch (error) {
+        console.error(error)
+    }
+})
 
 module.exports = router;
