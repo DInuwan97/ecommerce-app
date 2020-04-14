52bb9c72ef176edefc7a56208094e4401dee7892
DInuwan97 <kalubowila.dc25132@live.com>
2020-04-14 16:03:12 +0530

    Admin Home Page Added

---
+++ adminclient/src/components/DirectChat/DirectChat.jsx
@@ -0,0 +1,260 @@
+import React, { Component } from 'react';
+
+export default class DirectChat extends Component {
+  render() {
+    return (
+
+        <div className="row">
+        <div className="col-md-6">
+
+
+        <div className="card direct-chat direct-chat-primary">
+        <div className="card-header">
+          <h3 className="card-title">Direct Chat</h3>
+
+          <div className="card-tools">
+            <span data-toggle="tooltip" title="3 New Messages" className="badge badge-primary">3</span>
+            <button type="button" className="btn btn-tool" data-card-widget="collapse">
+              <i className="fas fa-minus"></i>
+            </button>
+            <button type="button" className="btn btn-tool" data-toggle="tooltip" title="Contacts"
+                    data-widget="chat-pane-toggle">
+              <i className="fas fa-comments"></i>
+            </button>
+            <button type="button" className="btn btn-tool" data-card-widget="remove"><i className="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+
+        <div className="card-body">
+         
+          <div className="direct-chat-messages">
+
+            <div className="direct-chat-msg">
+              <div className="direct-chat-infos clearfix">
+                <span className="direct-chat-name float-left">Alexander Pierce</span>
+                <span className="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
+              </div>
+              
+              <img className="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image"/>
+             
+              <div className="direct-chat-text">
+                Is this template really for free? That's unbelievable!
+              </div>
+             
+            </div>
+         
+            <div className="direct-chat-msg right">
+              <div className="direct-chat-infos clearfix">
+                <span className="direct-chat-name float-right">Sarah Bullock</span>
+                <span className="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
+              </div>
+           
+              <img className="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image"/>
+             
+              <div className="direct-chat-text">
+                You better believe it!
+              </div>
+             
+            </div>
+
+            <div className="direct-chat-msg">
+              <div className="direct-chat-infos clearfix">
+                <span className="direct-chat-name float-left">Alexander Pierce</span>
+                <span className="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
+              </div>
+             
+              <img className="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image"/>
+          
+              <div className="direct-chat-text">
+                Working with AdminLTE on a great new app! Wanna join?
+              </div>
+             
+            </div>
+           
+
+          
+            <div className="direct-chat-msg right">
+              <div className="direct-chat-infos clearfix">
+                <span className="direct-chat-name float-right">Sarah Bullock</span>
+                <span className="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
+              </div>
+             
+              <img className="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image"/>
+              
+              <div className="direct-chat-text">
+                I would love to.
+              </div>
+             
+            </div>
+           
+
+          </div>
+
+          <div className="direct-chat-contacts">
+            <ul className="contacts-list">
+              <li>
+                <a href="#">
+                  <img className="contacts-list-img" src="dist/img/user1-128x128.jpg"/>
+
+                  <div className="contacts-list-info">
+                    <span className="contacts-list-name">
+                      Count Dracula
+                      <small className="contacts-list-date float-right">2/28/2015</small>
+                    </span>
+                    <span className="contacts-list-msg">How have you been? I was...</span>
+                  </div>
+                 
+                </a>
+              </li>
+              
+              <li>
+                <a href="#">
+                  <img className="contacts-list-img" src="dist/img/user7-128x128.jpg"/>
+
+                  <div className="contacts-list-info">
+                    <span className="contacts-list-name">
+                      Sarah Doe
+                      <small className="contacts-list-date float-right">2/23/2015</small>
+                    </span>
+                    <span className="contacts-list-msg">I will be waiting for...</span>
+                  </div>
+                 
+                </a>
+              </li>
+            
+              <li>
+                <a href="#">
+                  <img className="contacts-list-img" src="dist/img/user3-128x128.jpg"/>
+
+                  <div className="contacts-list-info">
+                    <span className="contacts-list-name">
+                      Nadia Jolie
+                      <small className="contacts-list-date float-right">2/20/2015</small>
+                    </span>
+                    <span className="contacts-list-msg">I'll call you back at...</span>
+                  </div>
+                  
+                </a>
+              </li>
+              
+              <li>
+                <a href="#">
+                  <img className="contacts-list-img" src="dist/img/user5-128x128.jpg"/>
+
+                  <div className="contacts-list-info">
+                    <span className="contacts-list-name">
+                      Nora S. Vans
+                      <small className="contacts-list-date float-right">2/10/2015</small>
+                    </span>
+                    <span className="contacts-list-msg">Where is your new...</span>
+                  </div>
+                 
+                </a>
+              </li>
+              
+              <li>
+                <a href="#">
+                  <img className="contacts-list-img" src="dist/img/user6-128x128.jpg"/>
+
+                  <div className="contacts-list-info">
+                    <span className="contacts-list-name">
+                      John K.
+                      <small className="contacts-list-date float-right">1/27/2015</small>
+                    </span>
+                    <span className="contacts-list-msg">Can I take a look at...</span>
+                  </div>
+                 
+                </a>
+              </li>
+             
+              <li>
+                <a href="#">
+                  <img className="contacts-list-img" src="dist/img/user8-128x128.jpg"/>
+
+                  <div className="contacts-list-info">
+                    <span className="contacts-list-name">
+                      Kenneth M.
+                      <small className="contacts-list-date float-right">1/4/2015</small>
+                    </span>
+                    <span className="contacts-list-msg">Never mind I found...</span>
+                  </div>
+                  
+                </a>
+              </li>
+            
+            </ul>
+           
+          </div>
+         
+        </div>
+      
+        <div className="card-footer">
+          <form action="#" method="post">
+            <div className="input-group">
+              <input type="text" name="message" placeholder="Type Message ..." className="form-control"/>
+              <span className="input-group-append">
+                <button type="button" className="btn btn-primary">Send</button>
+              </span>
+            </div>
+          </form>
+        </div>
+        
+      </div>
+
+      </div>
+
+
+
+
+      <div className="col-md-6">
+
+      <div className="card bg-gradient-success">
+              <div className="card-header border-0">
+
+                <h3 className="card-title">
+                  <i className="far fa-calendar-alt"></i>
+                  Calendar
+                </h3>
+               
+                <div class="card-tools">
+                 
+                  <div className="btn-group">
+                    <button type="button" className="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
+                      <i className="fas fa-bars"></i></button>
+                    <div className="dropdown-menu float-right" role="menu">
+                      <a href="#" className="dropdown-item">Add new event</a>
+                      <a href="#" className="dropdown-item">Clear events</a>
+                      <div className="dropdown-divider"></div>
+                      <a href="#" className="dropdown-item">View calendar</a>
+                    </div>
+                  </div>
+                  <button type="button" className="btn btn-success btn-sm" data-card-widget="collapse">
+                    <i className="fas fa-minus"></i>
+                  </button>
+                  <button type="button" className="btn btn-success btn-sm" data-card-widget="remove">
+                    <i className="fas fa-times"></i>
+                  </button>
+                </div>
+               
+              </div>
+            
+              <div className="card-body pt-0">
+               
+                <div id="calendar" style={{width: '100%'}}></div>
+              </div>
+             
+            </div>
+
+
+
+      </div>
+
+
+
+
+      </div>
+     
+    );
+  }
+}
