1eb0ad3590d5b3bc6611b3cf30ad3aefd2b1e67c
[sachin] <[sachin2262716@gmail.com]>
2020-04-19 22:41:36 +0530

    stucture cart and checkout

---
+++ client/src/Components/Cart/Cart.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import classes from "./Cart.module.css";
+
+class Cart extends Component {
+
+  render() {
+
+
+    return (
+      <div>Cart</div>
+    );
+  }
+}
+
+export default Cart;
---
+++ client/src/Components/Checkout/Checkout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import classes from "./Checkout.module.css";
+
+class Checkout extends Component {
+
+  render() {
+
+
+    return (
+      <div>Checkout</div>
+    );
+  }
+}
+
+export default Checkout;
