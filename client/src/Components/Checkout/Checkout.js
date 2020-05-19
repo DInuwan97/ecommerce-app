edadc1d969bce8209f2067f5719f635d19c5e4c7
[sachin] <[sachin2262716@gmail.com]>
2020-05-19 16:54:03 +0530

    quick update checkout.js

--- client/src/Components/Checkout/Checkout.js
+++ client/src/Components/Checkout/Checkout.js
@@ -65,33 +65,23 @@ class Checkout extends Component {
       method: 'get',
       url: `/api/users/singleUser/${loggedEmail}`
     })
-    .then(res=>{
-      ////console.log('Chekc oUT USER DATA : ',res.data);
-      const user = res.data;
-      const userBuyer = {
-        addedUserFirstName:res.data.firstName,
-        addedUserLastName:res.data.lastName,
-        addedUserAddress:res.data.address,
-        addedUserMobile:res.data.mobile,
-        addedUserEmail:res.data.email
-      }
-      this.setState({
-        buyer:userBuyer
+      .then(res => {
+        console.log('Chekc oUT USER DATA : ', res.data);
+        const user = res.data;
+        const userBuyer = {
+          addedUserFirstName: res.data.firstName,
+          addedUserLastName: res.data.lastName,
+          addedUserAddress: res.data.address,
+          addedUserMobile: res.data.mobile
+        }
+        this.setState({
+          buyer: userBuyer
+        });
+
+        console.log(this.state.buyer);
+        console.log("Check out firstname : ", this.state.buyer.addedUserEmail);
+
       })
-
-      const userDetails = {
-        firstName:res.data.firstName,
-        lastName:res.data.lastName,
-        address:res.data.address,
-        mobile:res.data.mobile,
-        email:res.data.email
-      }
-      this.setState({
-        buyerDetails:userDetails
-      })
-
-      console.log("Check out firstname : ",this.state.buyerDetails.email);
-
 
   }
 
@@ -100,7 +90,7 @@ class Checkout extends Component {
   changeDetails = (buyer) => {
     console.log(buyer);
     this.setState({ buyer: buyer, detailActive: false });
-  }
+  };
 
   changeCard = (field, value) => {
     console.log(field);
