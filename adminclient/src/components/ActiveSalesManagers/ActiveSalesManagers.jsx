dca29a47d9a40be92a6707284d1daf8b4f427986
DInuwan97 <kalubowila.dc25132@live.com>
2020-05-21 14:53:31 +0530

    Active Sales Managers Part Completed

--- adminclient/src/components/ActiveSalesManagers/ActiveSalesManagers.jsx
+++ adminclient/src/components/ActiveSalesManagers/ActiveSalesManagers.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-
+import Avatar from 'react-avatar';
 export default class ActiveSalesManagers extends Component {
 
     constructor(props){
         super(props);
+        this.state = {
+            usersList : []
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            usersList:this.props.usersList
+        })
     }
 
 
@@ -16,13 +25,10 @@ export default class ActiveSalesManagers extends Component {
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Contacts</h1>
+            <h1>Active Sales Managers / Companies</h1>
           </div>
           <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Contacts</li>
-            </ol>
+     
           </div>
         </div>
       </div>
@@ -34,38 +40,70 @@ export default class ActiveSalesManagers extends Component {
       <div class="card card-solid">
         <div class="card-body pb-0">
           <div class="row d-flex align-items-stretch">
-            <div class="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
-              <div class="card bg-light">
-                <div class="card-header text-muted border-bottom-0">
-                  Digital Strategist
-                </div>
-                <div class="card-body pt-0">
-                  <div class="row">
-                    <div class="col-7">
-                      <h2 class="lead"><b>Nicole Pearson</b></h2>
-                      <p class="text-muted text-sm"><b>About: </b> Web Designer / UX / Graphic Artist / Coffee Lover </p>
-                      <ul class="ml-4 mb-0 fa-ul text-muted">
-                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-building"></i></span> Address: Demo Street 123, Demo City 04312, NJ</li>
-                        <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> Phone #: + 800 - 12 12 23 52</li>
-                      </ul>
-                    </div>
-                    <div class="col-5 text-center">
-                      <img src="../../dist/img/user1-128x128.jpg" alt="" class="img-circle img-fluid"/>
-                    </div>
+
+          {this.state.usersList.map(({
+              _id,
+              firstName,
+              lastName,
+              adminVerification,
+              userImageUrl,
+              company,
+              address,
+              email,
+              mobile,
+              regDate
+          }) => {
+              if(adminVerification === true || adminVerification === false){
+                        
+                    return (
+                       
+            <div className="col-12 col-sm-6 col-md-4 d-flex align-items-stretch">
+            <div className="card bg-light">
+              <div className="card-header text-muted border-bottom-0">
+                {company}
+              </div>
+              <div className="card-body pt-0">
+                <div className="row">
+                  <div className="col-7">
+                    <h2 className="lead"><b>{firstName} {lastName}</b></h2>
+                    <p className="text-muted text-sm"><b>Date of Joined: </b> {regDate} </p>
+                    <ul className="ml-4 mb-0 fa-ul text-muted">
+                      <li className="small"><span class="fa-li"><i className="fas fa-lg fa-building"></i></span> Address: {address}</li>
+                      <li className="small"><span class="fa-li"><i className="fas fa-lg fa-phone"></i></span> Phone #: {mobile}</li>
+                    </ul>
                   </div>
-                </div>
-                <div class="card-footer">
-                  <div class="text-right">
-                    <a href="#" class="btn btn-sm bg-teal">
-                      <i class="fas fa-comments"></i>
-                    </a>
-                    <a href="#" class="btn btn-sm btn-primary">
-                      <i class="fas fa-user"></i> View Profile
-                    </a>
+                  <div className="col-5 text-center">
+
+                    {(userImageUrl === '') &&
+                        <Avatar name={firstName+ ' ' +lastName}                         
+                        round="50%"
+                        size="140" />
+                    }
+
+                    {(userImageUrl !== '')&&
+                    <img src={userImageUrl} alt="" className="img-circle img-fluid"/>
+                    }
                   </div>
                 </div>
               </div>
+              <div className="card-footer">
+                <div className="text-right">
+                  <a href="#" className="btn btn-sm bg-teal">
+                    <i className="fas fa-envelope"></i>
+                  </a>
+                  <a href="#" className="btn btn-sm btn-primary" style={{marginLeft:5}}>
+                    <i className="fas fa-user"></i> View Profile
+                  </a>
+                </div>
+              </div>
             </div>
+          </div>            
+                    );
+              }
+
+         
+          })}
+
    
           </div>
         </div>
