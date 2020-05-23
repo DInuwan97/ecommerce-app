d8ae931a90267fa290a72f9ff4f90990709ee0e3
[sachin] <[sachin2262716@gmail.com]>
2020-05-24 04:47:04 +0530

    spinners addes to adminclient

--- adminclient/src/components/DataTables/SalesManagerAccess/SalesManagerFunctions.jsx
+++ adminclient/src/components/DataTables/SalesManagerAccess/SalesManagerFunctions.jsx
@@ -1,79 +1,84 @@
-import React, { Component } from 'react';
-import SalesServicersList from './SalesServicersList';
+import React, { Component } from "react";
+import SalesServicersList from "./SalesServicersList";
+import Spinner from "../../Spinner/Spinner";
+import axios from "axios";
 
-import axios from 'axios';
-
-
-const $ = require('jquery');
-$.DataTable = require('datatables.net');
+const $ = require("jquery");
+$.DataTable = require("datatables.net");
 export default class SalesManagerFunctions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      salesServicersList: [],
+      isLoading: true,
+    };
+  }
 
-    
-      constructor(props) {
-        super(props);
-            this.state = {
-            salesServicersList: []
-        };
-      }
-    
-      componentDidMount() {
+  componentDidMount() {
+    axios({
+      method: "get",
+      url: "/api/users/viewusers",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userLoginToken"),
+      },
+    }).then((res) => {
+      const users = res.data;
+      this.setState({
+        salesServicersList: users,
+        isLoading: false,
+      });
+      $("#salesServicersList-table").DataTable();
+    });
+  }
 
-        axios({
-            method:'get',
-            url:'/api/users/viewusers',
-            headers: {
-                "Authorization" : "Bearer "+localStorage.getItem('userLoginToken')
-            }
-        })
-        .then((res) => {
-          const users = res.data;
-          this.setState({
-            salesServicersList: users,
-          });
-          $('#salesServicersList-table').DataTable()
-        });
-      }
+  approveSalesServicer = (email) => {
+    this.setState({
+      salesServicersList: [
+        ...this.state.salesServicersList.filter((user) => user.email !== email),
+      ],
+    });
+    axios({
+      method: "patch",
+      url: `/api/users/confirmSalesServicer/${email}`,
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userLoginToken"),
+      },
+    })
+      .then((res) => {})
+      .catch((err) => {});
+  };
 
-      approveSalesServicer = (email) => {
-        this.setState({
-            salesServicersList: [...this.state.salesServicersList.filter((user) => user.email !== email)],
-         });
-         axios({
-             method:'patch',
-             url:`/api/users/confirmSalesServicer/${email}`,
-             headers:{
-                "Authorization" : "Bearer "+localStorage.getItem('userLoginToken')
-             }
-         })
-         .then(res=>{
-         })
-         .catch(err=>{
-         });
-      }
-
-
-      deleteSalesServicer = (email) =>{
-         this.setState({
-          salesServicersList: [...this.state.salesServicersList.filter((user) => user.email !== email)]
-         })
-         axios({
-          method:'delete',
-          url:`/api/users/deleteSalesManager/${email}`,
-          headers: {
-              "Authorization" : "Bearer "+localStorage.getItem('userLoginToken')
-          }
-        })
-        .then((res) => {
-        })
-        .catch((err) =>{
-        });
-      }
-        
+  deleteSalesServicer = (email) => {
+    this.setState({
+      salesServicersList: [
+        ...this.state.salesServicersList.filter((user) => user.email !== email),
+      ],
+    });
+    axios({
+      method: "delete",
+      url: `/api/users/deleteSalesManager/${email}`,
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userLoginToken"),
+      },
+    })
+      .then((res) => {})
+      .catch((err) => {});
+  };
 
   render() {
     const { companyName } = this.props;
-    return (
-        <SalesServicersList users={this.state.salesServicersList} deleteSalesServicer={this.deleteSalesServicer} approveSalesServicer = {this.approveSalesServicer} companyName={companyName}/>
+    let content = (
+      <SalesServicersList
+        users={this.state.salesServicersList}
+        deleteSalesServicer={this.deleteSalesServicer}
+        approveSalesServicer={this.approveSalesServicer}
+        companyName={companyName}
+      />
     );
+    if (this.state.isLoading) {
+      content = <Spinner />;
+    }
+
+    return <div>{content}</div>;
   }
 }
--- adminclient/src/components/DataTables/UserListDataRow.jsx
+++ adminclient/src/components/DataTables/UserListDataRow.jsx
@@ -1,101 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import swal from "sweetalert";
-export default function({id,
-    firstName,
-    lastName,
-    email,
-    mobile,
-    adminVerification,
-    company,
-    packageName,
-    approveSalesManagerRequest,
-    deleteSalesManager}) {
+export default function ({
+  id,
+  firstName,
+  lastName,
+  email,
+  mobile,
+  adminVerification,
+  company,
+  packageName,
+  approveSalesManagerRequest,
+  deleteSalesManager,
+}) {
+  return (
+    <tr>
+      <td>
+        {firstName} {lastName}
+      </td>
+      <td>{company}</td>
+      <td>{email}</td>
+      <td>{mobile}</td>
+      <td>{packageName}</td>
+      {() => {
+        if (adminVerification) {
+          return (
+            <td>
+              <button class="btn btn-info btn-sm mr-1">Approve</button>
+              <button class="btn btn-danger btn-sm">Decline</button>
+            </td>
+          );
+        } else {
+          return (
+            <td>
+              <button class="btn btn-info btn-sm mr-1">Approve</button>
+              <button class="btn btn-danger btn-sm">Decline</button>
+            </td>
+          );
+        }
+      }}
 
-    
-        return (
-                 <tr>
-                  <td>{firstName} {' '} {lastName}</td>
-                  <td>{company}</td>
-                  <td>{email}</td>
-                  <td>{mobile}</td>
-                  <td>{packageName}</td>
-                {
-                (()=>{
-                    if(adminVerification){
-                        return(
-                        <td>
-                            <button class="btn btn-info btn-sm mr-1">Approve</button>
-                            <button class="btn btn-danger btn-sm">Decline</button>
-                        </td>
-                        )
-                    }else{
-                        return(
-                        <td>
-                            <button class="btn btn-info btn-sm mr-1">Approve</button>
-                            <button class="btn btn-danger btn-sm">Decline</button>
-                        </td>
-                        )
-                    }
-                }
-                )}
-
-                    <td>
-                            <button 
-                              onClick={() =>
-                                swal({
-                                  title: "Are you sure?",
-                                  text: "Do you want to approve?",
-                                  icon: "warning",
-                                  buttons: true,
-                                  dangerMode: true,
-                                })
-                                .then((willApprove) => {
-                                  if (willApprove) {
-
-                                    swal("The Sales Manager has been Approved", {
-                                      icon: "success",
-                                    }).then(() => {
-                                         approveSalesManagerRequest(email)
-                                    })
-                                
-                                  } else {
-                                    swal("The Sales Manager Not Yet Approved");
-                                  }
-                                })
-                              }
-                             class="btn btn-info btn-sm mr-1">
-                                 Approve
-                            </button>
-                            <button 
-                             onClick={() =>
-                              swal({
-                                title: "Are you sure?",
-                                text: "Do you want to Delete?",
-                                icon: "warning",
-                                buttons: true,
-                                dangerMode: true,
-                              })
-                              .then((willDelete) => {
-                                if (willDelete) {
-
-                                  swal("The Sales Manager has been Deleted", {
-                                    icon: "success",
-                                    button:true
-                                  }).then(() => {
-                                    deleteSalesManager(email)
-                                  })
-                              
-                                } else {
-                                  swal("The Sales Manager Not Yet Approved");
-                                }
-                              })
-                            }
-                            class="btn btn-danger btn-sm"
-                            
-                            >Decline</button>
-                        </td>
-                </tr>
-      
-        )
-    
+      <td>
+        <button
+          onClick={() =>
+            swal({
+              title: "Are you sure?",
+              text: "Do you want to approve?",
+              icon: "warning",
+              buttons: true,
+              dangerMode: true,
+            }).then((willApprove) => {
+              if (willApprove) {
+                swal("The Sales Manager has been Approved", {
+                  icon: "success",
+                }).then(() => {
+                  approveSalesManagerRequest(email);
+                });
+              } else {
+                swal("The Sales Manager Not Yet Approved");
+              }
+            })
+          }
+          class="btn btn-info btn-sm mr-1"
+        >
+          Approve
+        </button>
+        <button
+          onClick={() =>
+            swal({
+              title: "Are you sure?",
+              text: "Do you want to Delete?",
+              icon: "warning",
+              buttons: true,
+              dangerMode: true,
+            }).then((willDelete) => {
+              if (willDelete) {
+                swal("The Sales Manager has been Deleted", {
+                  icon: "success",
+                  button: true,
+                }).then(() => {
+                  deleteSalesManager(email);
+                });
+              } else {
+                swal("The Sales Manager Not Yet Approved");
+              }
+            })
+          }
+          class="btn btn-danger btn-sm"
+        >
+          Decline
+        </button>
+      </td>
+    </tr>
+  );
 }
